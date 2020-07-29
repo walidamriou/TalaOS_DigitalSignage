0985489825c04effb31a87435366652f055cc1de
Walid Amriou <walidamriou@gmail.com>
2020-07-30 01:21:26 +0200

    Create talaos_ds_data/config.js

---
+++ talaos_ds_data/config.js
@@ -0,0 +1,18 @@
+/* 
+
+TalaOS Digital Signage - Config   
+
+Developed by: Walid Amriou
+www.walidamriou.com
+
+This work is licensed under:
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+
+Last update: July 2020
+
+*/
+
+var screen_size;
+var transitions_speed;
+var images_type;
+var images_number;
