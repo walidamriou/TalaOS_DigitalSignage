48b54dd4a4af0f59a652a7c34ebbeb6ae192835a
Walid Amriou <walidamriou@gmail.com>
2020-07-30 15:14:11 +0200

    core/scripts/main.js add config valures

--- core/scripts/main.js
+++ core/scripts/main.js
@@ -12,15 +12,20 @@ Last update: July 2020
 
 */
 
-let transitions_speed = 2000 //ms
-let image_number = 9
+//let screen_size_width,screen_size_height,transitions_speed,images_type,images_number = config()
+
+let screen_size_width=config_valures["screen_size_width_valure"]
+let screen_size_height=config_valures["screen_size_height_valure"]
+let transitions_speed=config_valures["transitions_speed_valure"]
+let images_type=config_valures["images_type_valure"]
+let image_numbero=config_valures["images_number_valure"]
 
 // create images
-for (let index = 1; index < image_number + 1; index++) {
+for (let index = 1; index < image_numbero + 1; index++) {
     let img = document.createElement("img")
-    img.src = "../talaos_ds_data/resources/" + index + ".png"
+    img.src = "../talaos_ds_data/resources/" + index + "."+images_type
     img.className = "talaos_ds_image"
-    img.style.cssText = 'width:100%;height:100%'
+    img.style.cssText = "width:"+screen_size_width+"px;height:"+screen_size_height+"px"
 
     let screen = document.getElementById("screen");
     screen.appendChild(img)
@@ -32,11 +37,11 @@ talaos_ds_display_images_loop()
 
 function talaos_ds_display_images_loop() {
     let talaos_ds_image = document.getElementsByClassName("talaos_ds_image")
-    for (let index = 0; index < image_number; index++) {
+    for (let index = 0; index < image_numbero; index++) {
         talaos_ds_image[index].style.display = "none"
     }
     images_index++
-    if (images_index > image_number) {
+    if (images_index > image_numbero) {
         images_index = 1
     }
     talaos_ds_image[images_index - 1].style.display = "block"
